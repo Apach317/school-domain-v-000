bd9da93569629efbc5918ef3992abbef0900acfd
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-06 22:37:00 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,14 +5,16 @@ class School
 
   def initialize(school)
     @school = school
+    roster(name, grade) = {
 
+    }
   end
 
   def school
     @school
-    roster = {
-
-    }
   end
+
+  def add_student(name, grade)
+
 
 end
