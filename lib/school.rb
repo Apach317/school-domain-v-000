fc59c7521466cfaa9c424233e8ac6e074310d258
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-06 22:46:59 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -18,8 +18,21 @@ class School
   end
 
   def add_student(name, grade)
-
+    if @roster.keys.include?(grade)
+      @roster[grade] << [name]
+    else
+      @roster[grade] = [name]
+    end
   end
 
+  def grade(stud_grade)
+    @roster[stud_grade]
+  end
+
+  def sort
+    @roster.each do |new_grade, student|
+      student.sort
+    end
+  end
 
 end
