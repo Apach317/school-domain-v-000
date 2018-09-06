150b6c479e2147b618ff0f178beedc76616d866e
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-06 21:52:07 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
 # code here!
+
+class School
+  attr_accessor :grade, :class
+  attr_reader :school
+  def initialize(school)
+    @school = school
+  end
+
+  def add_student
+    @school = school.new
+  end
+  
